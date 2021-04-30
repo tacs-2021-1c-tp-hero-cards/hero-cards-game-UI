d2c0ed1a06fe20c3a4576c0151107bc67d6702fc
FrancoEsposta <franco.esposta@despegar.com>
2021-04-30 09:36:10 -0300

    Agrego Chakra (en serio) y hago limpieza de cosas innecesarias metiendo base de chakra y manejo basico de urls

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,26 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Box } from '@chakra-ui/layout';
+import React from 'react'
+import { Home } from './components/Home'
+import { Login } from './components/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+  return resolveURL()
 }
+
+function resolveURL() {
+  switch (window.location.pathname) {
+    case '/':
+      return(<Home />);
+    case '/home':
+      return(<Home />);
+    case '/login':
+      return(<Login />);
+    default:
+      return(
+        <Box>Page not found!</Box>
+      )
+  }
+}
+
 
 export default App
