9f07b79adf57e1ad656b6c2c83e2721a1dea1835
FrancoEsposta <franco.esposta@despegar.com>
2021-05-10 21:46:57 -0300

    Mejoro las validaciones en el sign up del usuario

--- src/commons/InputValidations.tsx
+++ src/commons/InputValidations.tsx
@@ -1,29 +1,55 @@
 
 
-// TODO: Impprove inputs validations
-
 export function validateUsername(value: any) {
+    const usernameRegex = /^[a-zA-Z0-9]+$/
+    const atLeastOneLetterRegex = /[a-zA-Z]+/
     let error
+
     if (!value) {
         error = 'Username is required'
+    } else if(!usernameRegex.test(value)) {
+        error = 'Only letters and numbers are allowed'
+    } else if(!atLeastOneLetterRegex.test(value)) {
+        error = 'Username must have at least one letter'
     }
 
     return error
 }
 
 export function validateFullName(value: any) {
+    const fullNameRegex = /^[a-zA-Z]+(\s[a-zA-Z]+)*$/
     let error
+
     if (!value) {
         error = 'Full name is required'
+    } else if(!fullNameRegex.test(value)) {
+        error = 'It should only be formed by letters, maybe with spaces'
     }
 
     return error
 }
 
 export function validatePassword(value: any) {
+    const passwordMinLength = 6
+    const passwordMaxLength = 20
+    const passwordRegex = /^[a-zA-Z0-9]+$/
+    const atLeastOneLowerCaseRegex = /[a-z]+/
+    const atLeastOneUpperCaseRegex = /[A-Z]+/
+    const atLeastOneNumberRegex = /\d+/
     let error
+
     if (!value) {
         error = 'Password is required'
+    } else if(!passwordRegex.test(value)) {
+        error = 'Only letters and numbers are allowed'
+    } else if(value.length < passwordMinLength) {
+        error = `Password must have at least ${passwordMinLength} characters`
+    } else if(value.length > passwordMaxLength) {
+        error = `Password must have no more than ${passwordMaxLength} characters`
+    } else if(!(atLeastOneLowerCaseRegex.test(value) && 
+                atLeastOneUpperCaseRegex.test(value) && 
+                atLeastOneNumberRegex.test(value))) {
+                    error = 'Password must have, at least, an upper case letter, a lower case letter and a number'
     }
 
     return error
