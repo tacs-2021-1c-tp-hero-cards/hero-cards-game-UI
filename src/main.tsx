f6e58f7b9aeb320f7de1db396de1a89a9096bd45
FrancoEsposta <franco.esposta@despegar.com>
2021-07-13 00:26:14 -0300

    Agrego capa de persistencia para el estado de redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ChakraProvider } from '@chakra-ui/react'
+import { persistStore } from 'redux-persist'
 
 import App from './App'
 import theme from './theme'
+import store from './store/Store'
+import { PersistGate } from 'redux-persist/integration/react'
+
+const persistor = persistStore(store)
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />        
+      </PersistGate>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/store/Store.tsx
+++ src/store/Store.tsx
@@ -1,6 +1,18 @@
-import { createStore, Store } from "redux";
+import { createStore, Store } from "redux"
 import rootReducer from "./Reducer"
+import storage from "redux-persist/lib/storage"
+import autoMergeLevel1 from "redux-persist/es/stateReconciler/autoMergeLevel1"
+import { persistReducer } from 'redux-persist'
 
-const store: Store = createStore(rootReducer)
+
+export const config = {
+    key: 'root',
+    storage: storage,
+    stateReconciler: autoMergeLevel1
+}
+
+const persisted = persistReducer(config, rootReducer)
+
+const store: Store = createStore(persisted)
 
 export default store
