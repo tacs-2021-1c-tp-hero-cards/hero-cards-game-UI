b84ee05b005f03366332c5293f806cd071e8a71e
FrancoEsposta <franco.esposta@despegar.com>
2021-07-18 17:29:18 -0300

    Corrijo el banner de login

--- src/components/session/LogInBanner.tsx
+++ src/components/session/LogInBanner.tsx
@@ -10,11 +10,15 @@ function LogInBannerContent({ redirect }: BannerProps) {
     return (
         <Stack backgroundColor='yellow.100' padding='0.5rem' borderRadius='0.5rem'>
             <Stack direction='row' alignSelf='center' spacing='4px' fontSize='xl'>
-                <Text>It seem that you're not logged into any account. You can</Text>
-                <Button variant='link' colorScheme='teal' fontWeight='thin' fontSize='xl' onClick={() => redirect('/logIn')}>log in</Button>
-                <Text>here. Or</Text>
-                <Button variant='link' colorScheme='teal' fontWeight='thin' fontSize='xl' onClick={() => redirect('/signUp')}>create an account</Button>
-                <Text>here.</Text>
+                <Text>It seem that you're not logged into any account. You can log in</Text>
+                <Stack direction='row' alignSelf='center' spacing='0px' fontSize='xl'>
+                    <Button variant='link' colorScheme='teal' fontWeight='thin' fontSize='xl' onClick={() => redirect('/logIn')}>here</Button>
+                    <Text>. Or create an account</Text>
+                </Stack>
+                <Stack direction='row' alignSelf='center' spacing='0px' fontSize='xl'>
+                    <Button variant='link' colorScheme='teal' fontWeight='thin' fontSize='xl' onClick={() => redirect('/signUp')}>here</Button>
+                    <Text>.</Text>
+                </Stack>
             </Stack>
         </Stack>
     )
